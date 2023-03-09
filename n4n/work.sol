78af8aed89eb539789e9a2e0014f34aafd694df8
Farhad Zada <farhad.szd@gmail.com>
2023-03-09 12:21:41 +0400

    Version 4

--- n4n/work.sol
+++ n4n/work.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 
 contract Work {
     address public immutable owner;
+
     constructor() {
         owner = msg.sender;
     }
+
+    function name() public returns (uint) {}
 }
